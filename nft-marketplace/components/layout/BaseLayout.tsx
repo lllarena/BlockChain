ff263bb9e864b01d115b11c19a901d6f2731db74
lllarena <larena0402@163.com>
2023-07-15 20:32:02 +0100

    BaseLayout

---
+++ nft-marketplace/components/layout/BaseLayout.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+import { ReactNode } from "react";
+import Navbar from "../navbar";
+
+/* React18 */
+interface BaseLayoutProps {
+    children?: ReactNode;
+}
+
+const BaseLayout: FunctionComponent<BaseLayoutProps> = ({ children }: BaseLayoutProps) => {
+
+    return (
+        <>
+            <Navbar />
+            <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+                <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default BaseLayout;
