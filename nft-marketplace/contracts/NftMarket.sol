54c628492303b2238637144c91be7f3d631f0ffc
lllarena <larena0402@163.com>
2023-07-20 19:35:44 +0100

    update mint token

--- nft-marketplace/contracts/NftMarket.sol
+++ nft-marketplace/contracts/NftMarket.sol
@@ -2,7 +2,76 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
+    //计数器功能来增加增量并获取当前值
+    using Counters for Counters.Counter;
+
+    struct NftItem {
+        uint tokenId;
+        uint price;
+        address creator;
+        bool isListed;
+    }
+
+    //上架价格
+    uint public listingPrice = 0.025 ether;
+
+    Counters.Counter private _listedItems; //how many items in nft market
+    Counters.Counter private _tokenIds; //how many nfts in total
+
+    mapping(string => bool) private _usedTokenURIs; //Check the validity of a URI
+    mapping(uint => NftItem) private _idToNftItem;
+
+    event NftItemCreated(
+        uint tokenId,
+        uint price,
+        address creator,
+        bool isListed
+    );
+
     constructor() ERC721("CreaturesNFT", "CNFT") {}
+
+    function getNftItem(uint tokenId) public view returns (NftItem memory) {
+        return _idToNftItem[tokenId];
+    }
+
+    function listedItemsCount() public view returns (uint) {
+        return _listedItems.current();
+    }
+
+    function tokenURIExists(string memory tokenURI) public view returns (bool) {
+        return _usedTokenURIs[tokenURI] == true;
+    }
+
+    function mintToken(
+        string memory tokenURI,
+        uint price
+    ) public payable returns (uint) {
+        require(!tokenURIExists(tokenURI), "Token URI already exists");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        //创建新的NFT来创建新的token
+        _tokenIds.increment();
+        _listedItems.increment();
+
+        uint newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId); //创建新的nft
+        _setTokenURI(newTokenId, tokenURI); //给新创建的nft设置uri
+        _createftItem(newTokenId, price);
+        _usedTokenURIs[tokenURI] = true;
+
+        return newTokenId;
+    }
+
+    function _createftItem(uint tokenId, uint price) private {
+        require(price > 0, "Price must be at least 1 wei");
+        _idToNftItem[tokenId] = NftItem(tokenId, price, msg.sender, true);
+        emit NftItemCreated(tokenId, price, msg.sender, true);
+    }
 }
